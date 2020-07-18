7e7e1d422d6fd422ba7b42f08b4003ebcd33a5e3
a.loginov <a.loginov@sakh.com>
2020-07-18 11:35:42 +1100

    380. List Categories

--- src/main/java/guru/springfamework/services/CategoryServiceImpl.java
+++ src/main/java/guru/springfamework/services/CategoryServiceImpl.java
@@ -3,10 +3,15 @@ package guru.springfamework.services;
 import guru.springfamework.api.v1.mapper.CategoryMapper;
 import guru.springfamework.api.v1.model.CategoryDTO;
 import guru.springfamework.repositories.CategoryRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Created by jt on 9/26/17.
+ */
+@Service
 public class CategoryServiceImpl implements CategoryService {
 
     private final CategoryMapper categoryMapper;
@@ -19,16 +24,15 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public List<CategoryDTO> getAllCategories() {
-        return categoryRepository
-            .findAll()
-            .stream()
-            .map(categoryMapper::categoryToCategoryDTO)
-            .collect(Collectors.toList());
+
+        return categoryRepository.findAll()
+                .stream()
+                .map(categoryMapper::categoryToCategoryDTO)
+                .collect(Collectors.toList());
     }
 
     @Override
-    public CategoryDTO getCategoryName(String name) {
-        return categoryMapper
-            .categoryToCategoryDTO(categoryRepository.findByName(name));
+    public CategoryDTO getCategoryByName(String name) {
+        return categoryMapper.categoryToCategoryDTO(categoryRepository.findByName(name));
     }
 }
--- src/test/java/guru/springfamework/services/CategoryServiceTest.java
+++ src/test/java/guru/springfamework/services/CategoryServiceTest.java
@@ -9,11 +9,10 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import javax.print.attribute.standard.MediaSize;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
@@ -21,11 +20,11 @@ public class CategoryServiceTest {
 
     public static final Long ID = 2L;
     public static final String NAME = "Jimmy";
-
     CategoryService categoryService;
 
     @Mock
     CategoryRepository categoryRepository;
+
 
     @Before
     public void setUp() throws Exception {
@@ -35,40 +34,38 @@ public class CategoryServiceTest {
     }
 
     @Test
-    public void getAllCategories() {
+    public void getAllCategories() throws Exception {
+
         //given
-        List<Category> categories = Arrays.asList(
-            new Category(),
-            new Category(),
-            new Category()
-        );
+        List<Category> categories = Arrays.asList(new Category(), new Category(), new Category());
 
         when(categoryRepository.findAll()).thenReturn(categories);
 
         //when
-        List<CategoryDTO> categoryDTOS =
-            categoryService.getAllCategories();
+        List<CategoryDTO> categoryDTOS = categoryService.getAllCategories();
 
         //then
         assertEquals(3, categoryDTOS.size());
+
     }
 
     @Test
-    public void getCategoryName() {
+    public void getCategoryByName() throws Exception {
+
         //given
         Category category = new Category();
         category.setId(ID);
         category.setName(NAME);
 
-        when(categoryRepository.findByName(anyString()))
-            .thenReturn(category);
+        when(categoryRepository.findByName(anyString())).thenReturn(category);
 
         //when
-        CategoryDTO categoryDTO = categoryService.getCategoryName(NAME);
+        CategoryDTO categoryDTO = categoryService.getCategoryByName(NAME);
 
         //then
         assertEquals(ID, categoryDTO.getId());
         assertEquals(NAME, categoryDTO.getName());
 
     }
+
 }
